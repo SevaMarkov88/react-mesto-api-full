80dcfe0fd1b8d24e025a11ef96e8b0c878a2090e
SevaMarkov88 <vsevolodmarkoff@yandex.ru>
2022-04-04 14:10:12 +0300

    fixed

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -148,7 +148,7 @@ module.exports.login = (req, res, next) => {
           httpOnly: true,
           sameSite: true,
         });
-      res.status(201).send(user);
+      res.status(201).send(user.email);
     })
     .catch(next);
 };
