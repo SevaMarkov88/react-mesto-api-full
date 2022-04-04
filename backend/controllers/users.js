84c49b6dc3990ea2ab4f40e3611e9671b21cff00
SevaMarkov88 <vsevolodmarkoff@yandex.ru>
2022-04-04 14:06:39 +0300

    readme update

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -148,7 +148,7 @@ module.exports.login = (req, res, next) => {
           httpOnly: true,
           sameSite: true,
         });
-      res.status(201).send(token);
+      res.status(201).send(user);
     })
     .catch(next);
 };
