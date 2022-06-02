98865bc0cb5dc562224d182269ddf2cd25240a9d
Seva Markov <88732338+SevaMarkov88@users.noreply.github.com>
2022-06-02 16:19:58 +0300

    Update auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,17 +1,27 @@
+require('dotenv').config();
+
+const { JWT_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
-const UnauthorizedError = require('../errors/UnauthorizedError');
+const UnauthorizedError = require('../errors/unauthorized-error');
+const errorMessages = require('../errors/messages');
 
-module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
+const auth = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new UnauthorizedError(errorMessages.unauthorized);
+  }
+
+  const token = authorization.replace('Bearer ', '');
   let payload;
-
   try {
-    payload = jwt.verify(token, process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'some-secret-key');
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    next(new UnauthorizedError('Необходима авторизация'));
+    throw new UnauthorizedError(errorMessages.unauthorized);
   }
 
   req.user = payload;
-
   next();
 };
+
+module.exports = auth;
